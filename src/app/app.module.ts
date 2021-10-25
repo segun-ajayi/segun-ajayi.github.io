dff20dd047447720128e0499e9f87685ef6774d7
segun ajayi <segun_aj@yahoo.com>
2021-10-25 03:16:05 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroductionComponent } from './Components/introduction/introduction.component';
+import { AboutComponent } from './Components/about/about.component';
+import { SkillsComponent } from './Components/skills/skills.component';
+import { ExperienceComponent } from './Components/experience/experience.component';
+import { EducationComponent } from './Components/education/education.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroductionComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
